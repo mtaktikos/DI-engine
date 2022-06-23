006ed983277a37b4e1811824d13408c1775ff5db
hiha3456 <744762298@qq.com>
2022-06-23 12:00:38 +0800

    add 1st version of fake_obs

--- dizoo/distar/envs/__init__.py
+++ dizoo/distar/envs/__init__.py
@@ -2,4 +2,4 @@ from .distar_env import DIStarEnv
 from .meta import *
 from .static_data import RACE_DICT, BEGIN_ACTIONS, ACTION_RACE_MASK, SELECTED_UNITS_MASK, ACTIONS
 from .stat import Stat
-from .fake_data import get_fake_rl_trajectory, fake_rl_data_batch_with_last
+from .fake_data import get_fake_rl_trajectory, fake_rl_data_batch_with_last, fake_obs
